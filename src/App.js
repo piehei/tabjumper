b7e5f28a33e0b40484e5dce04043eec4780f8269
Pietari Heino <ph@extreg.com>
2018-09-25 09:00:30 +0300

    refactor to Vue

---
+++ src/App.js
@@ -0,0 +1,141 @@
+export default {
+  name: 'App',
+  el: '#app',
+  data() {
+    return {
+      msg: 'msgmsg',
+      text: '',
+      originalTabs: [],
+      tabNum: 0,
+      windowNum: 0,
+      sIndex: 0,
+    }
+  },
+  watch: {
+    text: (nText) => {
+      console.log(nText);
+      chrome.history.search({ text: nText }, (results) => {
+        console.log('QUERY RESULTS');
+        console.log(results);
+      })
+    }
+  },
+  mounted() {
+    console.log('mounted App.vue');
+
+    this.updateTabs();
+
+    this.$nextTick(() => {
+      console.log(this.$refs)
+      this.$refs.input.focus()
+    })
+
+    window.document.addEventListener('keyup', this.nextItem);
+    window.document.addEventListener('visibilitychange', (evt) => {
+      console.log('VUE:: vis change')
+    })
+
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+      console.log('VUE:: msg');
+      console.log(message);
+      if (message.type === 'activated') {
+        this.updateTabs();
+        this.$refs.input.focus();
+        this.text = '';
+        this.sIndex = 0;
+      }
+    })
+
+
+  },
+  computed: {
+    tabs() {
+      if (this.text === '') {
+        return this.originalTabs;
+      }
+
+      const comp = this.text.toLowerCase();
+
+      const urlStart = this.originalTabs.filter(tab => tab.simpleUrl.startsWith(comp));
+      const urlIncludes = this.originalTabs
+        .filter(tab => tab.simpleUrl.includes(comp))
+        .sort((t1, t2) => t1.simpleUrl.indexOf(comp) - t2.simpleUrl.indexOf(comp))
+
+      const titleStart = this.originalTabs.filter(tab => tab.simpleTitle.startsWith(comp));
+      const titleIncludes = this.originalTabs
+        .filter(tab => tab.simpleTitle.includes(comp))
+        .sort((t1, t2) => t1.simpleTitle.indexOf(comp) - t2.simpleTitle.indexOf(comp))
+
+      const candidates = []
+        .concat(urlStart)
+        .concat(urlIncludes)
+        .concat(titleStart)
+        .concat(titleIncludes)
+
+      return this.unique(candidates)
+    },
+  },
+  methods: {
+
+    unique(arr) {
+      return arr.filter((value, index, array) => array.indexOf(value) === index)
+    },
+
+    updateTabs() {
+      console.log('VUE:: updateTabs');
+      chrome.tabs.query({}, (tabs) => {
+        console.log('VUE:: tabs received');
+        this.originalTabs = tabs
+          .filter(tab => !tab.url.includes("tabJump.html"))
+          .map(tab => Object.assign({}, tab, {
+            simpleUrl: this.formatUrl(tab.url),
+            simpleTitle: tab.title.toLowerCase().replace(" ", "")
+          }))
+
+        this.tabNum = this.originalTabs.length;
+        this.windowNum = this.originalTabs.map(t => t.windowId).filter((v, i, a) => a.indexOf(v) === i).length;
+      })
+    },
+
+    formatUrl(url) {
+      let sUrl = url;
+      if (url.includes("chrome-extension") && url.includes("&uri")) {
+        sUrl = url.split("&uri=")[1];
+      }
+      return sUrl.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "");
+    },
+
+    nextItem(evt) {
+      // up
+      if (evt.keyCode === 38) {
+        this.sIndex -= 1;
+        if (this.sIndex < 0) {
+          this.sIndex = this.tabs.length - 1;
+        }
+
+      // down
+      } else if (evt.keyCode === 40) {
+        this.sIndex += 1;
+        if (this.sIndex === this.tabs.length) {
+          console.log(this.tabs.length);
+          this.sIndex = 0;
+        }
+
+      // enter
+      } else if (evt.keyCode === 13) {
+        this.chooseTab(this.tabs[this.sIndex]);
+      }
+    },
+
+    chooseTab(tab) {
+      console.log('chooseTab');
+      console.log(tab);
+
+      chrome.runtime.sendMessage({
+        type: 'activate',
+        tab: tab
+      });
+    },
+
+  }
+}
--- src/back.js
+++ src/back.js
@@ -1,12 +1,11 @@
 
+let appTab = null;
 
 function activate(tab) {
-
-  console.log('activate')
-  console.log(tab)
-  console.log(tab.id)
+  console.log('activating previous tab');
   chrome.windows.update(tab.windowId, { focused: true })
   chrome.tabs.update(tab.id, { highlighted: true, active: true })
+  chrome.tabs.sendMessage(tab.id, { type: 'activated' });
 }
 
 
@@ -14,19 +13,42 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   console.log('back.js')
   console.log(message)
   console.log(sender)
-
   if (message.type === 'activate') {
     activate(message.tab);
     return;
   }
-  
-  chrome.tabs.insertCSS(sender.tab.id, {
-    file: "css.css"
-  })
+})
 
-  chrome.tabs.query({}, function(tabs) {
-    console.log(tabs)
-    chrome.tabs.sendMessage(sender.tab.id, {"tabs": tabs})
-    // sendResponse(tabs)
-  })
+const handleCommand = () => {
+  console.log('COMMAND CALLED');
+  if (!appTab) {
+    console.log('PREV TAB NOT FOUND')
+    chrome.tabs.create({
+      active: true,
+      url: 'tabJump.html'
+    }, (newTab) => {
+      console.log('CREATED');
+      console.log(newTab);
+      appTab = newTab;
+    })
+  } else {
+    console.log('PREV TAB: found!!!!!!!!!!!!')
+    activate(appTab);
+  }
+};
+
+
+
+chrome.commands.onCommand.addListener(cmd => {
+  handleCommand();
+})
+
+chrome.tabs.onRemoved.addListener((tabId, obj) => {
+  console.log('onRemoved listener');
+  console.log(tabId)
+  console.log(obj)
+  if (appTab && appTab.id === tabId) {
+    console.log('appTab closed')
+    appTab = null;
+  }
 })
